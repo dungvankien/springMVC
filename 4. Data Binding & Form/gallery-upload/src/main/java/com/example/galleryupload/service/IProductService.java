8965aff5925a8a0b4f7070161ab81e0f79db9bdd
dungvankien <vankienhue@gmail.com>
2022-10-04 09:50:20 +0700

    uploadfile

---
+++ 4. Data Binding & Form/gallery-upload/src/main/java/com/example/galleryupload/service/IProductService.java
@@ -0,0 +1,17 @@
+package com.example.galleryupload.service;
+
+import com.example.galleryupload.model.Product;
+
+import java.util.List;
+
+public interface IProductService {
+    List<Product> findAll();
+
+    void save(Product product);
+
+    Product findById(int id);
+
+    void update(int id, Product product);
+
+    void remove(int id);
+}
